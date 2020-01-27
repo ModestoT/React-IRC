48917bf0f0db36c7362e8015a1a1373b4a6f2c4d
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2020-01-26 21:51:33 -0600

    initial code for front end

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect, useRef} from 'react';
+import io from "socket.io-client";
+
 import './App.css';
 
+const socket = io.connect("http://localhost:3001");
 function App() {
+  const [data, setData] = useState([]);
+  const divRef = useRef(null);
+
+  useEffect(() => {
+    const connectToServer = () => {
+      socket.emit("connect to irc", "drol", null);
+      socket.on("irc connection", newData => {
+        console.log(typeof newData);
+        if(typeof newData === 'string'){
+          setData(d => [...d, newData]);
+          divRef.current.scrollTop = divRef.current.scrollHeight;
+        } else {
+          console.log(newData)
+        }
+      });
+    
+      socket.on("disconnect", reason => {
+        console.log(reason);
+        socket.connect();
+      });
+    }
+    connectToServer();
+  },[]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="irc-chat" style={{width:'50%', height:'300px', overflow:'auto', wordBreak:'break-word', margin:'5% auto', border:'1px solid', padding:'1%'}} ref={divRef}>
+        {data.map(line => {
+          return <p>{line}</p>
+        })}
+      </div>
     </div>
   );
 }
