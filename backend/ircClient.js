26cb9986519dee3b97c0915965558913fc9a8a77
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2020-02-04 13:27:26 -0600

    Added emits to frontend for when someone leaves channel/server.

--- backend/ircClient.js
+++ backend/ircClient.js
@@ -1,14 +1,21 @@
 const IRC = require("irc-framework");
+const {formatQuitMessage} = require("./helperFunctions.js");
 
 module.exports = createIrcClient = socket => {
   const client = new IRC.Client();
+  let channel;
 
   client.on('socket connected', () => {
     console.log("IRC Socket connected Registering...");
   }).on('connected', e => {
     console.log("Connected: \n", e);
     console.log("joining channel...");
-    client.join("#horriblesubs");
+    channel = client.channel('#Rizon');
+    channel.updateUsers(channel => {
+      socket.emit("users list", {channelName: channel.name, users: channel.users});
+    })
+  }).on('channel list', list => {
+    console.log('Channel list: ', list);
   }).on('debug', e => {
     console.log("debug: ", e);
   }).on('join', e => {
@@ -20,12 +27,19 @@ module.exports = createIrcClient = socket => {
     console.log("Topic event: ", e);
   }).on('part', e => {
     console.log("Part Event: ", e);
+    socket.emit("left channel", {...e, message: formatQuitMessage(e.message)});
   }).on('quit', e => {
     console.log("Quit event", e);
+    // let channelList = []; for keeping track of what channels the user is in when they quit the server
+    // loop through all the channels the user is apart of and if the user that quit the server is
+    // in a channel append it to the list and send the list of channels to the frontend
+
+    socket.emit("left channel", {...e, channel: channel.name});
   }).on('invited', e => {
     console.log("Invite event: ", e);
   }).on('notice', e => {
     console.log("Notice event: ", e);
+    //do this on front end to properly display BOLD and color assignments /u00002 = bold /u0003 = blue
     let newData = e.message;
     if(newData.includes("\u0002")){
       const tempData = newData.replace(/\u0002/g, "").replace(/\u0003/g, "").replace(/11/g, "");
@@ -44,7 +58,7 @@ module.exports = createIrcClient = socket => {
   }).on('ctcp request', e => {
     console.log("CTCP Request event", e);
   }).on('motd', e => {
-    console.log("MOTD event: ", e);
+    // console.log("MOTD event: ", e);
     let newData = e.motd;
     if(newData.includes("\u0002")){
       const tempData = newData.replace(/\u0002/g, "").replace(/\u0003/g, "").replace(/11/g, "");
