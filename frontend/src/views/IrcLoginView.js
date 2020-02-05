07d1a9735c1a720b7f5cfa825b610ea19fa89366
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2020-02-05 16:50:09 -0600

    Updated to now have a submit button/function and to control it's own state for inputs. Updated variables to now target new state object

--- frontend/src/views/IrcLoginView.js
+++ frontend/src/views/IrcLoginView.js
@@ -1,44 +1,47 @@
 import React from "react";
+
+import { useFormInput } from "../customHooks/useFormInput.js";
 import InputField from "../components/InputField.js";
 
-const IrcLoginView = ({ ircOptions, handleInput }) => {
-  const {
-    host, 
-    port, 
-    nick, 
-    ssl 
-  } = ircOptions;
+const IrcLoginView = ({ connectToIrc }) => {
+  const [ircOptions, handleInput] = useFormInput({
+    host: 'irc.rizon.net',
+    port: 6697,
+    nick: 'drol',
+    ssl: true
+  });
   return(
-    <form className="irc-login-form">
+    <form className="irc-login-form" onSubmit={(e) => connectToIrc(ircOptions, e)}>
       <InputField 
         type={"text"} 
         labelText={"Server"} 
         className={"irc-hostname"} 
-        value={host} 
+        value={ircOptions.host} 
         onChange={e => handleInput({host: e.target.value})}
       />
       <InputField 
         type={"number"} 
         labelText={"Port"} 
         className={"irc-port"} 
-        value={port} 
+        value={ircOptions.port} 
         onChange={e => handleInput({port: e.target.value})}
       />
       <InputField 
         type={"text"} 
         labelText={"Nickname"} 
         className={"irc-nick"} 
-        value={nick} 
+        value={ircOptions.nick} 
         onChange={e => handleInput({nick: e.target.value})}
       />
       <InputField 
         type={"checkbox"} 
         labelText={"Secure Connection"} 
         className={"irc-ssl"} 
-        checked={ssl} 
+        checked={ircOptions.ssl} 
         value={"Secure Connection"}
-        onChange={() => handleInput({ssl: !ssl})}
+        onChange={() => handleInput({ssl: !ircOptions.ssl})}
       />
+      <button className="connect">Connect</button>
     </form>
   );
 };
