e5c7c66385882b6d9f50df68de4e86ce59c63ef7
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2020-02-07 16:21:55 -0600

    updated to now support tabs

--- frontend/src/views/IrcChatView.js
+++ frontend/src/views/IrcChatView.js
@@ -1,43 +1,78 @@
 import React, { useState } from "react";
 
-import IrcChatTab from "../components/irc/IrcChatTab";
 import Modal from "../components/Modal";
 import IrcJoinableChannels from "../components/irc/IrcJoinableChannels";
+import IrcChatTabs from "../components/irc/IrcChatTabs";
+import IrcChat from "../components/irc/IrcChat";
 
-const IrcChatView = ({ state, joinIrcChannel, grabAvailableChannels, searchForChannel, resetSearchResArray }) => {
-  const [isToggled, setIsToggled] = useState(false);
-  const { serverName, serverMsgs, userChannels, joinableChannels, isGrabbingChannels, searchRes } = state;
+const IrcChatView = ({
+	state,
+	joinIrcChannel,
+	grabAvailableChannels,
+	searchForChannel,
+	resetSearchResArray
+}) => {
+	const {
+		serverName,
+		serverMsgs,
+		userChannels,
+		joinableChannels,
+		isGrabbingChannels,
+		searchRes
+	} = state;
+	const [isToggled, setIsToggled] = useState(false);
+	const [currentTab, setCurrentTab] = useState(serverName);
 
-  const toggleModal = () => {
-    if(joinableChannels.pages === 0 ){
-      grabAvailableChannels();
-    }
+	const toggleModal = () => {
+		if (joinableChannels.pages === 0) {
+			grabAvailableChannels();
+		}
 
-    setIsToggled(!isToggled);
-  }
+		setIsToggled(!isToggled);
+	};
+	const getCurrentTabChat = () => {
+		if (currentTab === serverName) {
+			return <IrcChat chatMsgs={serverMsgs} />;
+		} else {
+			const chat = userChannels.find(
+				({ channelName }) => channelName === currentTab
+			);
 
-  return(
-    <div>
-      <IrcChatTab tabName={serverName} chatMsgs={serverMsgs}/>
-      {userChannels.map(channel => {
-        return(
-          <IrcChatTab key={channel.channelName} tabName={channel.channelName} chatMsgs={channel.messages}/>
-        );
-      })}
-      <button className="join-channel" onClick={() => toggleModal()}>+</button>
-      <Modal showModal={isToggled} toggleModal={setIsToggled}>
-        <IrcJoinableChannels
-          joinableChannels={joinableChannels} 
-          joinIrcChannel={joinIrcChannel} 
-          isGrabbingChannels={isGrabbingChannels} 
-          searchRes={searchRes}
-          searchForChannel={searchForChannel}
-          resetSearchResArray={resetSearchResArray}
-        />
-      </Modal>
-    </div>
-  );
+			if (chat) {
+				return <IrcChat chatMsgs={chat.messages} />;
+			} else {
+				return <IrcChat chatMsgs={[]} />;
+			}
+		}
+	};
+	const handleJoinIrcChannel = channelName => {
+		joinIrcChannel(channelName);
+		setCurrentTab(channelName);
+	};
+	return (
+		<div>
+			<IrcChatTabs
+				tabs={[{ channelName: serverName }, ...userChannels]}
+				serverName={serverName}
+				currentTab={currentTab}
+				setCurrentTab={setCurrentTab}
+			/>
+			{getCurrentTabChat()}
+			<button className="join-channel" onClick={() => toggleModal()}>
+				+
+			</button>
+			<Modal showModal={isToggled} toggleModal={setIsToggled}>
+				<IrcJoinableChannels
+					joinableChannels={joinableChannels}
+					joinIrcChannel={handleJoinIrcChannel}
+					isGrabbingChannels={isGrabbingChannels}
+					searchRes={searchRes}
+					searchForChannel={searchForChannel}
+					resetSearchResArray={resetSearchResArray}
+				/>
+			</Modal>
+		</div>
+	);
 };
-
 
 export default IrcChatView;
