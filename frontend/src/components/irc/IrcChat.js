a25a10d62c4013afc86eb87036aadd9fc75ba26e
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2020-02-07 19:44:53 -0600

    added userLists to component and some basic css

--- frontend/src/components/irc/IrcChat.js
+++ frontend/src/components/irc/IrcChat.js
@@ -1,30 +1,39 @@
 import React, { useEffect, useRef } from "react";
+import styled from "styled-components";
+
 import IrcChatMsg from "./IrcChatMsg";
 
-const IrcChat = ({ chatMsgs }) => {
+const IrcChatWindow = styled.div`
+	width: 80%;
+	overflow: auto;
+	word-break: break-word;
+	margin: 1% auto;
+	border: 1px solid;
+	padding: 1%;
+`;
+
+const IrcChat = ({ channel }) => {
+	const { messages, userList } = channel;
 	const divRef = useRef(null);
 
 	useEffect(() => {
 		divRef.current.scrollTop = divRef.current.scrollHeight;
-	}, [chatMsgs]);
+	}, [messages]);
 
 	return (
-		<div
-			className="irc-chat"
-			style={{
-				width: "50%",
-				height: "300px",
-				overflow: "auto",
-				wordBreak: "break-word",
-				margin: "5% auto",
-				border: "1px solid",
-				padding: "1%"
-			}}
-			ref={divRef}
-		>
-			{chatMsgs.map((msg, index) => {
-				return <IrcChatMsg key={index} msg={msg} />;
-			})}
+		<div style={{ display: "flex", height: "87vh" }}>
+			<IrcChatWindow ref={divRef}>
+				{messages.map((msg, index) => {
+					return <IrcChatMsg key={index} msg={msg} />;
+				})}
+			</IrcChatWindow>
+			{userList.length > 0 && (
+				<ul className="irc-chat-user-list" style={{ overflow: "auto" }}>
+					{userList.map(user => {
+						return <li key={user.nick}>{user.nick}</li>;
+					})}
+				</ul>
+			)}
 		</div>
 	);
 };
