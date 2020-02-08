7536395dda5dc3285f6664db06b3d48372fc7933
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2020-02-07 19:47:05 -0600

    Added case for updating user list for channels

--- frontend/src/customHooks/ircHook/IrcReducer.js
+++ frontend/src/customHooks/ircHook/IrcReducer.js
@@ -1,4 +1,7 @@
-import { createIrcConnection, grabServerName } from "../../helpers/IrcHelpers.js";
+import {
+	createIrcConnection,
+	grabServerName
+} from "../../helpers/IrcHelpers.js";
 
 export const CONNECTION_ESTABLISHED = "CONNECTION_ESTABLISHED";
 export const CONNECTION_LOST = "CONNECTION_LOST";
@@ -7,109 +10,158 @@ export const MOTD_MESSAGE = "MOTD_MESSAGE";
 export const CHANNEL_NOTICE = "CHANNEL_NOTICE";
 export const CHANNEL_MESSAGE = "CHANNEL_MESSAGE";
 export const MAKING_CONNECTION = "MAKING_CONNECTION";
-export const GRABBING_CHANNEL_LIST = 'GRABBING_CHANNEL_LIST';
-export const UPDATE_CHANNELS_COUNT = 'UPDATE_CHANNELS_COUNT';
-export const GRABBING_CHANNEL_LIST_END = 'GRABBING_CHANNEL_LIST_END';
-export const CHANNEL_PRV_MSG = 'CHANNEL_PRV_MSG';
+export const GRABBING_CHANNEL_LIST = "GRABBING_CHANNEL_LIST";
+export const UPDATE_CHANNELS_COUNT = "UPDATE_CHANNELS_COUNT";
+export const GRABBING_CHANNEL_LIST_END = "GRABBING_CHANNEL_LIST_END";
+export const CHANNEL_PRV_MSG = "CHANNEL_PRV_MSG";
+export const UPDATE_USERS_LIST = "UPDATE_USERS_LIST";
 
 export const IrcReducer = (state, action) => {
-  switch(action.type){
-    case MAKING_CONNECTION:
-      const ircSocket = createIrcConnection(action.payload);
+	switch (action.type) {
+		case MAKING_CONNECTION:
+			const ircSocket = createIrcConnection(action.payload);
 
-      return {
-        ...state,
-        serverName: grabServerName(action.payload.host),
-        ircSocket
-      }
-    case CONNECTION_ESTABLISHED:
-      console.log("connected to irc")
-      return {
-        ...state,
-        isConnected: true
-      };
-    case CONNECTION_LOST:
-      return {
-        serverName: "",
-        serverMsgs: [],
-        userChannels: [],
-        joinableChannels: {
-          pages: 0,
-          channels:[],
-          channelCount: 0
-        },
-        isConnected: false,
-        ircSocket: null
-      };
-    case GRABBING_CHANNEL_LIST: 
-      return {
-        ...state,
-        isGrabbingChannels: true
-      };
-    case UPDATE_CHANNELS_COUNT:
-      return {
-        ...state,
-        joinableChannels: {
-          pages: state.joinableChannels.pages + 1,
-          channelCount: state.joinableChannels.channelCount + action.payload
-        }
-      };
-    case GRABBING_CHANNEL_LIST_END:
-      return {
-        ...state,
-        isGrabbingChannels: false,
-        joinableChannels: {
-          ...state.joinableChannels,
-          channels: action.payload
-        }
-      };
-    case NOTICE_MESSAGE:
-      return {
-        ...state,
-        serverMsgs: [...state.serverMsgs, `-${action.payload.nick}- ${action.payload.message}`]
-      };
-    case MOTD_MESSAGE: 
-      return {
-        ...state,
-        serverMsgs: [...state.serverMsgs, action.payload]
-      };
-    case CHANNEL_NOTICE: 
-      return {
-        ...state,
-        userChannels: state.userChannels.map(channel => channel.channelName === action.payload.channelName ? {...channel, messages: [...channel.messages, `-${action.payload.nick}- ${action.payload.message}`]} : channel)
-      };
-    case CHANNEL_MESSAGE:
-      const channel = state.userChannels.find(({ channelName }) => channelName === action.payload.channel);
-      if(channel){
-        const {nick, ident, hostname, status, channel, message} = action.payload;
-        return {
-          ...state,
-          userChannels: state.userChannels.map(c => 
-            c.channelName === channel ? 
-              {...c, messages: [...c.messages, `${nick} [${ident}@${hostname}] has ${status} ${channel} ${status === "left" ? `[${message}]` : ""}`]}
-            : c
-          )
-        };
-      } else {
-        return {
-          ...state,
-          userChannels: [...state.userChannels, {
-            channelName: action.payload.channel,
-            messages: [],
-            userList: [],
-          }]
-        }
-      };
-    case CHANNEL_PRV_MSG:
-      return {
-        ...state,
-        userChannels: state.userChannels.map(c => 
-          c.channelName === action.payload.target ? 
-            {...c, messages: [...c.messages, `<${action.payload.nick}> ${action.payload.message}`]}
-          : c
-        )
-      };
-    default:
-      return state;
-  }
-}
+			return {
+				...state,
+				serverName: grabServerName(action.payload.host),
+				ircSocket
+			};
+		case CONNECTION_ESTABLISHED:
+			console.log("connected to irc");
+			return {
+				...state,
+				isConnected: true
+			};
+		case CONNECTION_LOST:
+			return {
+				serverName: "",
+				serverMsgs: [],
+				userChannels: [],
+				joinableChannels: {
+					pages: 0,
+					channels: [],
+					channelCount: 0
+				},
+				isConnected: false,
+				ircSocket: null
+			};
+		case GRABBING_CHANNEL_LIST:
+			return {
+				...state,
+				isGrabbingChannels: true
+			};
+		case UPDATE_CHANNELS_COUNT:
+			return {
+				...state,
+				joinableChannels: {
+					pages: state.joinableChannels.pages + 1,
+					channelCount: state.joinableChannels.channelCount + action.payload
+				}
+			};
+		case GRABBING_CHANNEL_LIST_END:
+			return {
+				...state,
+				isGrabbingChannels: false,
+				joinableChannels: {
+					...state.joinableChannels,
+					channels: action.payload
+				}
+			};
+		case NOTICE_MESSAGE:
+			return {
+				...state,
+				serverMsgs: [
+					...state.serverMsgs,
+					`-${action.payload.nick}- ${action.payload.message}`
+				]
+			};
+		case MOTD_MESSAGE:
+			return {
+				...state,
+				serverMsgs: [...state.serverMsgs, action.payload]
+			};
+		case CHANNEL_NOTICE:
+			return {
+				...state,
+				userChannels: state.userChannels.map(channel =>
+					channel.channelName === action.payload.channelName
+						? {
+								...channel,
+								messages: [
+									...channel.messages,
+									`-${action.payload.nick}- ${action.payload.message}`
+								]
+						  }
+						: channel
+				)
+			};
+		case CHANNEL_MESSAGE:
+			const channel = state.userChannels.find(
+				({ channelName }) => channelName === action.payload.channel
+			);
+			if (channel) {
+				const {
+					nick,
+					ident,
+					hostname,
+					status,
+					channel,
+					message
+				} = action.payload;
+				return {
+					...state,
+					userChannels: state.userChannels.map(c =>
+						c.channelName === channel
+							? {
+									...c,
+									messages: [
+										...c.messages,
+										`${nick} [${ident}@${hostname}] has ${status} ${channel} ${
+											status === "left" ? `[${message}]` : ""
+										}`
+									]
+							  }
+							: c
+					)
+				};
+			} else {
+				return {
+					...state,
+					userChannels: [
+						...state.userChannels,
+						{
+							channelName: action.payload.channel,
+							messages: [],
+							userList: []
+						}
+					]
+				};
+			}
+		case CHANNEL_PRV_MSG:
+			return {
+				...state,
+				userChannels: state.userChannels.map(c =>
+					c.channelName === action.payload.target
+						? {
+								...c,
+								messages: [
+									...c.messages,
+									`<${action.payload.nick}> ${action.payload.message}`
+								]
+						  }
+						: c
+				)
+			};
+		case UPDATE_USERS_LIST:
+			return {
+				...state,
+				userChannels: state.userChannels.map(c =>
+					c.channelName === action.payload.channelName
+						? { ...c, userList: action.payload.users }
+						: c
+				)
+			};
+		default:
+			return state;
+	}
+};
--- frontend/src/customHooks/ircHook/useIrc.js
+++ frontend/src/customHooks/ircHook/useIrc.js
@@ -1,88 +1,111 @@
 import { useReducer, useEffect } from "react";
 
 import { parseForChannelName } from "../../helpers/IrcHelpers.js";
-import { 
-  IrcReducer, 
-  NOTICE_MESSAGE, 
-  CHANNEL_NOTICE, 
-  CHANNEL_MESSAGE, 
-  MOTD_MESSAGE, 
-  CONNECTION_ESTABLISHED, 
-  CONNECTION_LOST,
-  MAKING_CONNECTION, 
-  GRABBING_CHANNEL_LIST,
-  UPDATE_CHANNELS_COUNT,
-  GRABBING_CHANNEL_LIST_END,
-  CHANNEL_PRV_MSG
+import {
+	IrcReducer,
+	NOTICE_MESSAGE,
+	CHANNEL_NOTICE,
+	CHANNEL_MESSAGE,
+	MOTD_MESSAGE,
+	CONNECTION_ESTABLISHED,
+	CONNECTION_LOST,
+	MAKING_CONNECTION,
+	GRABBING_CHANNEL_LIST,
+	UPDATE_CHANNELS_COUNT,
+	GRABBING_CHANNEL_LIST_END,
+	CHANNEL_PRV_MSG,
+	UPDATE_USERS_LIST
 } from "./IrcReducer.js";
 
 export const useIrc = () => {
-  const [state, dispatch] = useReducer(IrcReducer,{
-    serverName: "",
-    serverMsgs: [],
-    userChannels: [],
-    joinableChannels: {
-      pages: 0,
-      channelCount: 0,
-      channels: []
-    },
-    isConnected: false,
-    isGrabbingChannels: false,
-    ircSocket: null
-  });
+	const [state, dispatch] = useReducer(IrcReducer, {
+		serverName: "",
+		serverMsgs: [],
+		userChannels: [],
+		joinableChannels: {
+			pages: 0,
+			channelCount: 0,
+			channels: []
+		},
+		isConnected: false,
+		isGrabbingChannels: false,
+		ircSocket: null
+	});
 
-  useEffect(() => {
-    if(state.ircSocket){
-      state.ircSocket.on("connected", () => {
-        dispatch({ type: CONNECTION_ESTABLISHED });
-      }).on("disconnect", reason => {
-        dispatch({ type: CONNECTION_LOST });
-        console.log("Diconnected from irc: ", reason);
-        state.ircSocket.close();
-      }).on("irc notice", notice => {
-        const channelName = parseForChannelName(notice.message);
-        
-        if(!channelName || channelName[0] !== "#"){
-          dispatch({ type: NOTICE_MESSAGE, payload: notice });
-        } else{
-          dispatch({ type: CHANNEL_NOTICE, payload: {...notice, channelName}});
-        }
-      }).on("joined channel", e => {
-        dispatch({ type: CHANNEL_MESSAGE, payload: {...e, status: "joined" } });
-      }).on("left channel", e => {
-        dispatch({ type: CHANNEL_MESSAGE, payload: {...e, status: "left" } });
-      }).on("server motd", motd => {
-        dispatch({ type: MOTD_MESSAGE, payload: motd });
-      }).on("grabbing channel list", () => {
-        dispatch({ type: GRABBING_CHANNEL_LIST });
-      }).on("available channels", count => {
-        dispatch({ type: UPDATE_CHANNELS_COUNT, payload: count });
-      }).on("grabbing channel list end", channels => {
-        dispatch({ type: GRABBING_CHANNEL_LIST_END, payload: channels });
-      }).on("channel prv msg", msg => {
-        dispatch({ type: CHANNEL_PRV_MSG, payload: msg });
-      });
-    }  
-  },[state.ircSocket]);
-  
-  const connectToIrc = (ircOptions, e) => {
-    e.preventDefault();
+	useEffect(() => {
+		if (state.ircSocket) {
+			state.ircSocket
+				.on("connected", () => {
+					dispatch({ type: CONNECTION_ESTABLISHED });
+				})
+				.on("disconnect", reason => {
+					dispatch({ type: CONNECTION_LOST });
+					console.log("Diconnected from irc: ", reason);
+					state.ircSocket.close();
+				})
+				.on("irc notice", notice => {
+					const channelName = parseForChannelName(notice.message);
 
-    dispatch({ type: MAKING_CONNECTION, payload: ircOptions });
-  };
+					if (!channelName || channelName[0] !== "#") {
+						dispatch({ type: NOTICE_MESSAGE, payload: notice });
+					} else {
+						dispatch({
+							type: CHANNEL_NOTICE,
+							payload: { ...notice, channelName }
+						});
+					}
+				})
+				.on("joined channel", e => {
+					dispatch({
+						type: CHANNEL_MESSAGE,
+						payload: { ...e, status: "joined" }
+					});
+				})
+				.on("left channel", e => {
+					dispatch({
+						type: CHANNEL_MESSAGE,
+						payload: { ...e, status: "left" }
+					});
+				})
+				.on("users list", data => {
+					dispatch({ type: UPDATE_USERS_LIST, payload: data });
+				})
+				.on("server motd", motd => {
+					dispatch({ type: MOTD_MESSAGE, payload: motd });
+				})
+				.on("grabbing channel list", () => {
+					dispatch({ type: GRABBING_CHANNEL_LIST });
+				})
+				.on("available channels", count => {
+					dispatch({ type: UPDATE_CHANNELS_COUNT, payload: count });
+				})
+				.on("grabbing channel list end", channels => {
+					dispatch({ type: GRABBING_CHANNEL_LIST_END, payload: channels });
+				})
+				.on("channel prv msg", msg => {
+					dispatch({ type: CHANNEL_PRV_MSG, payload: msg });
+				});
+		}
+	}, [state.ircSocket]);
 
-  const joinIrcChannel = channelName => {
-    state.ircSocket.emit("join channel", channelName);
-  };
+	const connectToIrc = (ircOptions, e) => {
+		e.preventDefault();
 
-  const grabAvailableChannels = () => {
-    state.ircSocket.emit("grab channel list");
-  }
+		dispatch({ type: MAKING_CONNECTION, payload: ircOptions });
+	};
 
-  return {
-    state,
-    connectToIrc,
-    joinIrcChannel,
-    grabAvailableChannels
-  };
-}
+	const joinIrcChannel = channelName => {
+		state.ircSocket.emit("join channel", channelName);
+	};
+
+	const grabAvailableChannels = () => {
+		state.ircSocket.emit("grab channel list");
+	};
+
+	return {
+		state,
+		connectToIrc,
+		joinIrcChannel,
+		grabAvailableChannels
+	};
+};
