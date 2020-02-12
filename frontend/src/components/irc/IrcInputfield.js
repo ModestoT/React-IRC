66de3259cd608da43f9aa7d3c383c79d58af8084
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2020-02-12 14:39:12 -0600

    added /commands

--- frontend/src/components/irc/IrcInputfield.js
+++ frontend/src/components/irc/IrcInputfield.js
@@ -1,16 +1,38 @@
 import React from "react";
 
 import { useFormInput } from "../../customHooks/useFormInput.js";
+import { FixChannelName } from "../../helpers/IrcHelpers.js";
 
-const IrcInputField = ({ currentChannel, sendMessageToChannel, nick }) => {
+const IrcInputField = ({ currentChannel, sendMessageToChannel, joinIrcChannel, nick }) => {
 	const [message, setMessage] = useFormInput("");
 
 	const handleMessageToChannel = e => {
 		e.preventDefault();
 
-		sendMessageToChannel(currentChannel, message, nick);
+		if (message[0] === "/") {
+			const action = message.split(" ");
+			switch (action[0]) {
+				case "/join":
+					if (action[1]) {
+						joinIrcChannel(FixChannelName(action[1]));
+					}
+					break;
+				case "/away":
+					break;
+				case "/back":
+					break;
+				default:
+					return null;
+			}
+		} else {
+			sendMessageToChannel(currentChannel, message, nick);
+		}
+
 		setMessage("");
 	};
+	//TODO: Add checks for / commands, ex: /join, /message to user(look into this)
+	//Implement ability to join server/channels from localstorage, add previously joined config to localstorage
+	//afterwords begin work on UI
 	return (
 		<form onSubmit={e => handleMessageToChannel(e)}>
 			<input
