0d59a07ce0a3f3684d8080fd4142e99544679974
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2020-08-12 16:44:53 -0500

    updated CSS and moved user preview card here to fix display problem when user was clicked.

--- frontend/src/components/irc/IrcChatUsersList.js
+++ frontend/src/components/irc/IrcChatUsersList.js
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import IrcChatUser from "./IrcChatUser";
+import Button from "../Button.js";
 
 const UsersList = styled.ul`
 	position: absolute;
 	width: 50%;
 	height: 84.5%;
 	background: ${(props) => props.theme.secondaryBg};
-	transform: translate(${(props) => (props.showUsers ? "85%" : "190%")});
+	transform: translate(${(props) => (props.showUsers ? "85%" : "185%")});
 	transition: 0.5s ease-in-out;
 	margin: 0;
 	padding: 2%;
@@ -15,7 +16,7 @@ const UsersList = styled.ul`
 	z-index: 5;
 
 	@media (min-width: 1024px) {
-		right: 0;
+		transform: translate(0);
 		position: initial;
 		width: 14%;
 		height: 98%;
@@ -23,7 +24,51 @@ const UsersList = styled.ul`
 	}
 `;
 
-const IrcChatUsersList = ({ userList, showUsers }) => {
+const UserPreview = styled.div`
+	display: flex;
+	align-items: flex-start;
+	flex-wrap: wrap;
+	width: 55%;
+	height: 15%;
+	background: black;
+	border: 1px solid;
+	position: absolute;
+	margin: 10px;
+	left: 40%;
+	top: calc(${(props) => props.yCoord}px - 50px);
+	z-index: 10;
+
+	h4 {
+		margin: 8px 15px;
+		width: 75%;
+	}
+
+	@media (min-width: 1024px) {
+		width: 15%;
+		height: 10%;
+		left: 82%;
+		top: calc(${(props) => props.yCoord}px + 10px);
+
+		h4 {
+			width: 82%;
+		}
+	}
+`;
+
+const PreviewClose = styled.span`
+	cursor: pointer;
+	margin: 6px 0;
+
+	&:hover {
+		transform: scale(1.4);
+	}
+
+	@media (min-width: 1024px) {
+		margin-top: 2%;
+	}
+`;
+
+const IrcChatUsersList = ({ userList, showUsers, windowWidthSize }) => {
 	const [currentUserSelected, setCurrentUserSelected] = useState("");
 	const [previewCoords, setPreviewCoords] = useState({});
 
@@ -34,20 +79,20 @@ const IrcChatUsersList = ({ userList, showUsers }) => {
 
 	if (userList.length > 0) {
 		return (
-			<UsersList showUsers={showUsers}>
-				{userList.map((user, index) => {
-					return (
-						<IrcChatUser
-							key={index}
-							user={user}
-							currentUserSelected={currentUserSelected}
-							showPreviewCard={showPreviewCard}
-							setCurrentUserSelected={setCurrentUserSelected}
-							previewCoords={previewCoords}
-						/>
-					);
-				})}
-			</UsersList>
+			<>
+				<UsersList showUsers={showUsers}>
+					{userList.map((user, index) => {
+						return <IrcChatUser key={index} user={user} showPreviewCard={showPreviewCard} />;
+					})}
+				</UsersList>
+				{currentUserSelected !== "" && (
+					<UserPreview yCoord={previewCoords.y}>
+						<h4>{currentUserSelected}</h4>
+						<PreviewClose onClick={() => setCurrentUserSelected("")}>x</PreviewClose>
+						<Button btnText="Message" />
+					</UserPreview>
+				)}
+			</>
 		);
 	}
 	return null;
