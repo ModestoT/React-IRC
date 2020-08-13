4466700ae76cb6ffc547d865288ca6c0a911c84e
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2020-08-13 15:20:52 -0500

    fix please

--- frontend/src/irc/CreateIrcConnection.js
+++ frontend/src/irc/CreateIrcConnection.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
 import { AddServerToStorage } from "../helpers/GeneralHelpers.js";
-const backend = process.env.BACKEND;
+const backend = process.env.BACKEND || "http://localhost:3001";
 
 const ConnectToIrc = (ircOptions) => {
 	console.log(`connecting to ${backend}`);
