724da80c84c20a63d22d9fc7d919b6536fa7c222
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2020-08-18 15:26:52 -0500

    Cleaned up code to be more readable added new component for displaying page arrows

--- frontend/src/components/irc/IrcJoinableChannels.js
+++ frontend/src/components/irc/IrcJoinableChannels.js
@@ -1,30 +1,48 @@
 import React, { useState } from "react";
+import styled from "styled-components";
 
 import { SearchChannelMatrix } from "../../helpers/IrcHelpers.js";
 import IrcChannelsTable from "./IrcChannelsTable.js";
+import ChangePageButtons from "../ChangePageButtons.js";
+
+const ServerSearch = styled.input`
+	input {
+		width: 95%;
+		background: ${(props) => props.theme.inputBg};
+		color: ${(props) => props.theme.mainText};
+		border-radius: 15px;
+		border: none;
+		padding: 1%;
+		outline: none;
+
+		@media (min-width: 1024px) {
+			padding: 0.5%;
+		}
+	}
+`;
 
 const IrcJoinableChannels = ({
 	joinableChannels,
 	joinIrcChannel,
 	isGrabbingChannels,
-	toggleModal
+	toggleModal,
 }) => {
 	const { pages, channels } = joinableChannels;
 
 	const [data, setData] = useState({
 		currentPage: 0,
 		searchRes: [],
-		isSearching: false
+		isSearching: false,
 	});
 
-	const sendSearchTerm = searchTerm => {
+	const sendSearchTerm = (searchTerm) => {
 		if (searchTerm.length > 0) {
 			setData({ ...data, isSearching: true, currentPage: 0 });
 
 			const searchRes = SearchChannelMatrix(channels, searchTerm, 200);
-			setData(d => ({ ...d, searchRes, isSearching: false }));
+			setData((d) => ({ ...d, searchRes, isSearching: false }));
 		} else {
-			setData(d => ({ ...d, searchRes: [] }));
+			setData((d) => ({ ...d, searchRes: [] }));
 		}
 	};
 
@@ -55,46 +73,35 @@ const IrcJoinableChannels = ({
 		}
 	};
 
+	const handlePageBack = () => {
+		setData((d) => ({
+			...d,
+			currentPage: d.currentPage === 1 ? d.currentPage : d.currentPage - 1,
+		}));
+	};
+
+	const handlePageForward = () => {
+		setData((d) => ({
+			...d,
+			currentPage: d.currentPage === pages ? d.currentPage : d.currentPage + 1,
+		}));
+	};
 	return (
 		<>
-			<input
+			<ServerSearch
 				type="text"
 				disabled={isGrabbingChannels}
-				onChange={e => sendSearchTerm(e.target.value)}
+				onChange={(e) => sendSearchTerm(e.target.value)}
 				aria-label="Search for a channel to join"
+				placeholder="Search for a channel to join"
 			/>
-			<div>
-				<span
-					onClick={() =>
-						setData(d => ({
-							...d,
-							currentPage:
-								d.currentPage === 1 ? d.currentPage : d.currentPage - 1
-						}))
-					}
-				>
-					{"<"}
-				</span>
-				<span>
-					{data.currentPage + 1}/{pages}
-				</span>
-				<span
-					onClick={() =>
-						setData(d => ({
-							...d,
-							currentPage:
-								d.currentPage === pages ? d.currentPage : d.currentPage + 1
-						}))
-					}
-				>
-					{">"}
-				</span>
-			</div>
-			{isGrabbingChannels ? (
-				<span>Getting List of Channels...</span>
-			) : (
-				renderChannels()
-			)}
+			<ChangePageButtons
+				handlePageBack={handlePageBack}
+				handlePageForward={handlePageForward}
+				currentPage={data.currentPage}
+				totalPages={pages}
+			/>
+			{isGrabbingChannels ? <span>Getting List of Channels...</span> : renderChannels()}
 		</>
 	);
 };
