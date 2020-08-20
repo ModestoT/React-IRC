c409cd680c0a3ae3aef76d40d1c7ec60db257e2a
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2020-08-20 13:37:56 -0500

    added new array to track users private messages

--- frontend/src/customHooks/ircHook/useIrc.js
+++ frontend/src/customHooks/ircHook/useIrc.js
@@ -26,6 +26,7 @@ export const useIrc = () => {
 			channels: [],
 		},
 		channelsToJoin: [],
+		privateMsgs: [],
 		pastServers: JSON.parse(localStorage.getItem("past_servers")) || [],
 		isConnected: false,
 		isConnectedToServer: false,
